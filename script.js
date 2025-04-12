5bff0e8d2c47d3f496172d042ff722d884161f09
xedfr12345 <111363750+xedfr12345@users.noreply.github.com>
2025-04-12 22:42:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("KOSANA script loaded");
-
-  const btn = document.getElementById("themeToggle");
-  btn.addEventListener("click", () => {
-    document.body.classList.toggle("light-theme");
-  });
-});
 document.getElementById("theme-toggle").addEventListener("click", function () {
   document.body.classList.toggle("dark-theme");
 
