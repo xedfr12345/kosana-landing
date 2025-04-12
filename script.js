a039b18db1274c52337e19774acfa8a20971b92e
xedfr12345 <111363750+xedfr12345@users.noreply.github.com>
2025-04-12 18:46:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("KOSANA script loaded");
+
+  const btn = document.getElementById("themeToggle");
+  btn.addEventListener("click", () => {
+    document.body.classList.toggle("light-theme");
+  });
 });
