c2dd180b75e4c8830a7c717cc616f7770996e03b
xedfr12345 <111363750+xedfr12345@users.noreply.github.com>
2025-04-12 22:26:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ document.addEventListener("DOMContentLoaded", () => {
     document.body.classList.toggle("light-theme");
   });
 });
+document.getElementById("theme-toggle").addEventListener("click", function () {
+  document.body.classList.toggle("dark-theme");
+
+  const btn = document.getElementById("theme-toggle");
+  const title = document.getElementById("title");
+
+  if (document.body.classList.contains("dark-theme")) {
+    btn.textContent = "☀️ Верни свет";
+    title.textContent = "Тёмная сторона активна...";
+  } else {
+    btn.textContent = "🌙 Тыкни меня";
+    title.textContent = "Добро пожаловать, KOSANA";
+  }
+});
