cf1a0cdff1f21a484d41a8188f6efcacc2845092
xedfr12345 <111363750+xedfr12345@users.noreply.github.com>
2025-04-12 23:00:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-document.getElementById("theme-toggle").addEventListener("click", function () {
-  document.body.classList.toggle("dark-theme");
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("theme-toggle").addEventListener("click", function () {
+    document.body.classList.toggle("dark-theme");
 
-  const btn = document.getElementById("theme-toggle");
-  const title = document.getElementById("title");
+    const btn = document.getElementById("theme-toggle");
+    const title = document.getElementById("title");
 
-  if (document.body.classList.contains("dark-theme")) {
-    btn.textContent = "☀️ Верни свет";
-    title.textContent = "Тёмная сторона активна...";
-  } else {
-    btn.textContent = "🌙 Тыкни меня";
-    title.textContent = "Добро пожаловать, KOSANA";
-  }
-});
+    if (document.body.classList.contains("dark-theme")) {
+      btn.textContent = "☀️ Верни свет";
+      title.textContent = "Тёмная сторона активна...";
+    } else {
+      btn.textContent = "🌙 Тыкни меня";
+      title.textContent = "Добро пожаловать, KOSANA";
+    }
+  });
