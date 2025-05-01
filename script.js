aa4ba523585a6ee2c32a84df8a1744c49cdc9948
l_Kesana_l <keeks__kosatkina@MacBook-Pro-l-Kesana-l.local>
2025-05-02 00:47:51 +0300

    Чутка js

---
+++ script.js
@@ -0,0 +1,14 @@
+const button = document.getElementById('callButton');
+const response = document.getElementById('responseText');
+
+const phrases = [
+    "Она занята созданием смысла.",
+    "Наблюдает за тобой.",
+    "Ответ придет, когда будешь готов.",
+    "Система активирована. Жди сигнала."
+];
+
+button.addEventListener('click', () => {
+    const randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
+    response.textContent = randomPhrase;
+});
